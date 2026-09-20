0ed481f82956344d37a979307c2493bf6f9b429d
Aastha Dhavan <aasthadhavan@gmail.com>
2026-09-20 12:36:01 +0530

    Create 27.java

---
+++ Advanced/Day4/27.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int ptr=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(val!=nums[i]){
+             nums[ptr]=nums[i];
+             ptr++;
+            }
+        }
+        return ptr;
+    }
+}
