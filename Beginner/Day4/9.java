521938394741ad714f35ba8915077db936029235
Aastha Dhavan <aasthadhavan@gmail.com>
2026-09-20 12:23:59 +0530

    Create 9.java

---
+++ Beginner/Day4/9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x;
+        double reverse=0;
+        while(n>0){
+           int remainder=n%10;
+           reverse=reverse*10+remainder;
+           n=n/10; 
+        }
+        if(x==reverse){
+            return true;
+        }
+        return false;
+    }
+}
